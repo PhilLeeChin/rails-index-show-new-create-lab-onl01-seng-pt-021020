c0dce97bc537cda43282933124f1b9071a950edc
Philip Lee-Chin <phil.leechin@outlook.com>
2020-06-23 00:42:57 +0000

    Done.

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -2,4 +2,17 @@ class CouponController < ApplicationController
   def index
     @coupons = Coupon.all
   end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new(coupon_code: params[:coupon_code], store: params[:store])
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
 end
